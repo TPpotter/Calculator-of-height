67add2ab8afab5a6da3faa50b7c35a96d3059b35
TPpotter <filinov1377@gmail.com>
2023-08-06 22:28:31 +1000

    update 3.1

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from functions import app
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    if request.method == 'GET':
+        return render_template('easter.html')
+
 
 app.run(host='localhost', port=8000, debug=True)
